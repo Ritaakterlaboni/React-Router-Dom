4b43f57417b809930fe7018de21717f8f8c59483
Ritaakterlaboni <ritaakterlaboni4@gmail.com>
2025-09-24 19:07:51 +0600

    all updates

--- src/Component/Page/Signup.jsx
+++ src/Component/Page/Signup.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div className='text-2xl flex justify-center items-center py-5'>Signup</div>
+    <div className='text-2xl flex items-center justify-center py-5'>Signup</div>
   )
 }
 
